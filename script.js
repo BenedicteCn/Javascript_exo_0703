58795a861066f0d7d674cee07af125200d17ea07
Benedicte Coulon <benedicte.coulon@wanadoo.fr>
2022-03-07 13:20:49 +0100

    fonct 2

--- script.js
+++ script.js
@@ -6,5 +6,15 @@ let footer = document.querySelector("footer");
 i = 0
 footer.addEventListener("click",function(){
   i++
-  console.log(`Et +1 click ! Il y a eu ${i} désormais clicks au total`);
+  console.log(`Et +1 click ! Il y a eu désormais ${i} clicks au total`);
 });
+
+//Fonctionnalité 2 :
+
+let navbarh = document.getElementById("navbarHeader");
+document.getElementsByClassName("navbar-toggler")[0].addEventListener("click", function(){
+  navbarh.classList.toggle("collapse");
+});
+
+
+//Fonctionnalité 3 :
