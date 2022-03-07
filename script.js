7837a6410254daa0197b50b0f86537ca87541674
Benedicte Coulon <benedicte.coulon@wanadoo.fr>
2022-03-07 12:07:29 +0100

    fonct 1

---
+++ script.js
@@ -0,0 +1,10 @@
+//Fonctionnalité 1
+
+//Fonctionnalité 1bis :
+
+let footer = document.querySelector("footer");
+i = 0
+footer.addEventListener("click",function(){
+  i++
+  console.log(`Et +1 click ! Il y a eu ${i} désormais clicks au total`);
+});
