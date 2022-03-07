e7981d289a45bc995a50497814c1474d718b5699
Benedicte Coulon <benedicte.coulon@wanadoo.fr>
2022-03-07 15:42:09 +0100

    fonct 3 & 4

--- script.js
+++ script.js
@@ -16,5 +16,22 @@ document.getElementsByClassName("navbar-toggler")[0].addEventListener("click", f
   navbarh.classList.toggle("collapse");
 });
 
-
 //Fonctionnalité 3 :
+
+let firstCard = document.getElementsByClassName("card mb-4 box-shadow")[0]
+
+document.getElementsByClassName("btn btn-sm btn-outline-secondary")[0].addEventListener("click", function(){
+firstCard.style.color = 'red'
+});
+
+//Fonctionnalité 4 :
+
+let secondCard = document.getElementsByClassName("card mb-4 box-shadow")[1]
+
+document.getElementsByClassName("btn btn-sm btn-outline-secondary")[1].addEventListener("click", function(){
+  if (secondCard.style.color === 'green'){
+    secondCard.style.color = '' ;
+  }else{
+    secondCard.style.color = 'green'
+  }
+});
